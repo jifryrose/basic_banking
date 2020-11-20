0a5a6a692c81466e4c980b2c4eacbb775445007c
jifryrose <54110752+jifryrose@users.noreply.github.com>
2020-11-20 21:31:45 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,58 @@
+
+<html>
+<head>
+    <title>Basic Banking System</title>
+    <link rel="shortcut icon" href="images/cm.png">
+   
+	 <link rel="stylesheet" href="index_button.css">
+	<style>
+	button{
+		background-color:#8cbed6;
+	}
+	body
+	{
+		text-align:center;
+	}
+		body{
+		background-image:url("images/11.jpg");
+		background-position: center;
+		background-repeat: no-repeat;
+		background-size: cover;
+	}
+	</style>
+</head>
+<body >
+	<div id="header">
+       <br>
+       <h1 style=" font-family:Times New Roman; font-size: 60px; color:#ff0000;text-shadow: 1px 1px black;">The Sparks Foundation </h1>
+       <h2 style="font-family:Times New Roman;font-size:50px;color:0000ff; text-shadow:1px 1px black">Graduate Rotational Internship Program</h2>
+       <h2 style=" font-family:Times New Roman; font-size: 50px;color:#000000;text-shadow: 1px 1px black;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Basic Banking System </h2>
+       <h3 style="font-family:Times New Roman;font-size:40px;color:#000000">by Jifry Rose.R.V.</h3>
+    </div>
+        <div id="section">
+            <table>
+                <tr></tr>
+                <tr><br>
+				<a href="getdetail.php">
+               <button class="primary_btn" id="button "type="button" href="getdetail.php">View users</button>
+                </a>
+               
+                </tr>
+                <tr>        
+               <br> <br> <br>
+			   <a href="transfer.php">
+			   <button class="primary_btn" id="button" type="button">Credit Transfer</button>
+               </a>
+               
+               </tr>
+			   
+			    <tr>        
+               <br> <br> <br>
+			   <a href="transaction.php">
+			   <button class="primary_btn" id="button" type="button">All Transaction</button>
+               </a>
+            </table>
+    </div>
+	           
+</body>
+</html>
